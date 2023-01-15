18b16106e7eec13c4d8ad252a44a96e84b8eefe6
Aaron Wantoch <aaron.wantoch@gmail.com>
2023-01-15 13:54:02 +0100

    Added test only for wav file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # GuitarTrainer
+App reads data from .wav file in fmt format
 
 to install mkl:
     sudo apt install intel-mkl
--- src/WavHandler.cpp
+++ src/WavHandler.cpp
@@ -46,26 +46,6 @@ void WavHandler::ProcessHeaders()
         uint64_t numSamples = wavHeader.ChunkSize / bytesPerSample; //How many samples are in the wav file
 
         filelength = GetFileSize();
-
-        /*
-        cout << "File is                    :" << filelength << " bytes." << endl;
-        cout << "RIFF header                :" << wavHeader.RIFF[0] << wavHeader.RIFF[1] << wavHeader.RIFF[2] << wavHeader.RIFF[3] << endl;
-        cout << "WAVE header                :" << wavHeader.WAVE[0] << wavHeader.WAVE[1] << wavHeader.WAVE[2] << wavHeader.WAVE[3] << endl;
-        cout << "FMT                        :" << wavHeader.fmt[0] << wavHeader.fmt[1] << wavHeader.fmt[2] << wavHeader.fmt[3] << endl;
-        cout << "Data size                  :" << wavHeader.ChunkSize << endl;
-
-        // Display the sampling Rate from the header
-        cout << "Sampling Rate              :" << wavHeader.SamplesPerSec << endl;
-        cout << "Number of bits used        :" << wavHeader.bitsPerSample << endl;
-        cout << "Number of channels         :" << wavHeader.NumOfChan << endl;
-        cout << "Number of bytes per second :" << wavHeader.bytesPerSec << endl;
-        cout << "Data length                :" << wavHeader.Subchunk2Size << endl;
-        cout << "Audio Format               :" << wavHeader.AudioFormat << endl;
-        // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-
-        cout << "Block align                :" << wavHeader.blockAlign << endl;
-        cout << "Data string                :" << wavHeader.Subchunk2ID[0] << wavHeader.Subchunk2ID[1] << wavHeader.Subchunk2ID[2] << wavHeader.Subchunk2ID[3] << endl;
-        */
     }
 }
 
