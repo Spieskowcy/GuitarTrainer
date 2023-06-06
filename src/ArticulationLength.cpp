845d515ab05059243a25b0a6f01226f7ddbd7b96
AaronWantoch <58564314+AaronWantoch@users.noreply.github.com>
2023-06-06 11:42:16 +0200

    Articulation Length
    * Articulation Length single sound
    
    * Minimal length
    
    * Calculate length in signal with multiple sounds

---
+++ src/ArticulationLength.cpp
@@ -0,0 +1,102 @@
+#include "ArticulationLength.hpp"
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+void ArticulationLength::CalculateLength() 
+{
+    for(int i=0; i<total; i++)
+    {
+        if(i<50)
+            continue;
+        
+        if(absRaw[i]>=STARTING_THRESHOLD && i-startIndex > ATTACK_TIME)
+        {
+            if(currentSound!=nullptr)
+            {
+                SaveSound(i);
+            }
+            notHearableSoundCounter=0;
+            hearable = true;
+            currentSound = new Sound();
+            startIndex=i;
+        }
+        else if(absRaw[i]>=CONTINUE_THRESHOLD && hearable)
+        {
+            notHearableSoundCounter=0;
+        }
+        else if(absRaw[i]<CONTINUE_THRESHOLD && hearable)
+        {
+            notHearableSoundCounter++;
+            if(notHearableSoundCounter > COUNTER_THRESHOLD)
+            {
+                hearable = false;
+                SaveSound(i);
+                
+            }
+        }
+    }
+}
+
+void ArticulationLength::SaveSound(int currentIndex)
+{
+    if(currentIndex-startIndex<MIN_LENGTH)
+    {
+        return;
+    }
+    currentSound->beginIndex=startIndex;
+    currentSound->beginSegment=startIndex/numberOfSamples;
+    currentSound->endIndex=currentIndex;
+    currentSound->endSegment=currentIndex/numberOfSamples;
+    currentSound->soundIndex=currentSoundIndex;
+    sounds.push_back(currentSound);
+    currentSound++;
+    currentSound=nullptr;
+}
+
+void ArticulationLength::ReadSegments() {
+	WavHandler handler(filePath.c_str());
+    
+    std::queue<Segment*> segments;
+    
+    int totalSegments=0;
+	while (!handler.FinishedReading())
+	{
+		Segment* seg = new Segment(numberOfSamples);
+		handler.ReadRawData(seg);
+		total+=numberOfSamples;
+        totalSegments++;
+        segments.push(seg);
+	}
+    
+    raw = new float[total];
+    absRaw = new float[total];
+    int currentIndex = 0;
+    for(int i=0; i<totalSegments; i++)
+    {
+        Segment* currentSegment = segments.front();
+        for(int j=0; j<numberOfSamples; j++)
+        {
+            raw[currentIndex] = currentSegment->rawSignal[j];
+            absRaw[currentIndex] = abs(raw[currentIndex]);
+            currentIndex++;
+        }
+        segments.pop();
+        delete currentSegment;
+    }
+}
+
+ArticulationLength::ArticulationLength(int numberOfSamples, std::string filePath)
+{
+    this->numberOfSamples = numberOfSamples;
+    this->filePath = filePath;
+    absRaw = new float[this->numberOfSamples];
+    ReadSegments();
+    CalculateLength();
+}
+
+ArticulationLength::~ArticulationLength()
+{
+    delete absRaw;
+}
---
+++ src/ArticulationLength.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include "Segment.hpp"
+#include "WavHandler.h"
+#include <vector>
+
+struct Sound {
+    int beginIndex;
+    int beginSegment;
+    int endIndex;
+    int endSegment;
+    int soundIndex;
+    
+    int Length()
+    {
+        return endIndex-beginIndex;
+    }
+};
+
+class ArticulationLength {
+private:
+    const float STARTING_THRESHOLD = 2000000000;
+    const float CONTINUE_THRESHOLD = 210000000;
+    const int COUNTER_THRESHOLD = 100;
+    const int MIN_LENGTH = 2000;
+    const int ATTACK_TIME = 2000;
+
+    int numberOfSamples;
+    int total = 0;
+    float* absRaw;
+    float* raw;
+    std::string filePath;
+
+    int currentSoundIndex = 0;
+    int notHearableSoundCounter = 0;
+    bool hearable = false;
+    Sound* currentSound = nullptr;
+    int startIndex;
+
+    void SaveSound(int currentIndex);
+public:
+    std::vector<Sound*> sounds;
+
+    void CalculateLength();
+    void ReadSegments();
+    ArticulationLength(int numberOfSamples, std::string filePath);
+    ~ArticulationLength();
+};
