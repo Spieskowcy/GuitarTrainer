9cce8c9340bcb4a775b2bece1c738b47ebc833ec
AaronWantoch <58564314+AaronWantoch@users.noreply.github.com>
2023-06-21 20:20:37 +0200

    Articulation fix (#13)
    * Make starting threshold set based on max amplitude in file
    
    * Add use case to unit tests

--- test/articulationLengthTest.cpp
+++ test/articulationLengthTest.cpp
@@ -11,8 +11,18 @@ TEST(articulationLengthCheck, 1){
 	Segment seg(1024);
 	ArticulationLength articulationLength(1024, "../rsc/a_p.wav");
 
-	ASSERT_EQ(articulationLength.sounds[0]->beginIndex,593);
-	ASSERT_EQ(articulationLength.sounds[0]->endIndex,18637);
+	ASSERT_EQ(articulationLength.sounds[0]->beginIndex,1698);
+	ASSERT_EQ(articulationLength.sounds[0]->endIndex,26035);
 }
 
+TEST(articulationLengthUseCase, 2){
+	Segment seg(1024);
+	ArticulationLength articulationLength1(1024, "../rsc/cw1.wav");
+	ArticulationLength articulationLength2(1024, "../rsc/cw1.wav");
 
+	ASSERT_EQ(articulationLength1.sounds.size(), articulationLength2.sounds.size());
+	for(int i=0; i<articulationLength1.sounds.size(); i++)
+	{
+		ASSERT_EQ(articulationLength1.sounds[i]->Length(), articulationLength2.sounds[i]->Length());
+	}
+}
