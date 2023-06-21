ba3f6bfedc2914acc96f4e07c84d0e7cd35a5ee9
Aaron Wantoch <aaron.wantoch@gmail.com>
2023-06-21 22:04:01 +0200

    Last fix I promise

--- test/articulationLengthTest.cpp
+++ test/articulationLengthTest.cpp
@@ -7,22 +7,39 @@
 #include <iostream>
 #include <string.h>
 
-TEST(articulationLengthCheck, 1){
-	Segment seg(1024);
-	ArticulationLength articulationLength(1024, "../rsc/a_p.wav");
 
-	ASSERT_EQ(articulationLength.sounds[0]->beginIndex,1698);
-	ASSERT_EQ(articulationLength.sounds[0]->endIndex,26035);
-}
-
-TEST(articulationLengthUseCase, 2){
+TEST(articulationLengthTest, 1){
 	Segment seg(1024);
 	ArticulationLength articulationLength1(1024, "../rsc/cw1.wav");
 	ArticulationLength articulationLength2(1024, "../rsc/cw1.wav");
-
+	
 	ASSERT_EQ(articulationLength1.sounds.size(), articulationLength2.sounds.size());
 	for(int i=0; i<articulationLength1.sounds.size(); i++)
 	{
 		ASSERT_EQ(articulationLength1.sounds[i]->Length(), articulationLength2.sounds[i]->Length());
 	}
+}
+
+TEST(articulationLengthCw, 1){
+	Segment seg(1024);
+	ArticulationLength articulationLength1(1024, "../rsc/cw1.wav");
+	ArticulationLength articulationLength2(1024, "../rsc/cw1test.wav");
+	ASSERT_NE(articulationLength1.sounds.size(), 0);
+	ASSERT_NE(articulationLength2.sounds.size(), 0);
+}
+
+TEST(articulationLengthCw, 2){
+	Segment seg(1024);
+	ArticulationLength articulationLength1(1024, "../rsc/cw2.wav");
+	ArticulationLength articulationLength2(1024, "../rsc/cw2test.wav");
+	ASSERT_NE(articulationLength1.sounds.size(), 0);
+	ASSERT_NE(articulationLength2.sounds.size(), 0);
+}
+
+TEST(articulationLengthCw, 3){
+	Segment seg(1024);
+	ArticulationLength articulationLength1(1024, "../rsc/cw3.wav");
+	ArticulationLength articulationLength2(1024, "../rsc/cw3rhythm.wav");
+	ASSERT_NE(articulationLength1.sounds.size(), 0);
+	ASSERT_NE(articulationLength2.sounds.size(), 0);
 }
